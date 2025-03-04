435998f7dadf1e08b4c8f16c6a59539168f66fff
Your Name <you@example.com>
2025-03-04 16:15:16 +0500

    Fix salutation

--- run.py
+++ run.py
@@ -13,15 +13,18 @@ router = Router()
 
 @router.message(F.content_type.in_({'new_chat_members'}))
 async def salutations_process(message: types.Message, bot: Bot):
-    salutate_message = await message.answer(LEXICON['/salutate'].format(str(message.from_user.first_name)))
-    await bot.send_message(chat_id=7396564931, text=LEXICON["/new_member"].format(str(message.from_user.username), str(message.from_user.first_name), str(datetime.now()), str(message.from_user.id) ))
+    for new_member in message.new_chat_members:
+        if new_member.is_bot or new_member.id == message.bot.id:
+            continue
+
+        salutate_message = await message.answer(LEXICON['/salutate'].format(str(new_member.first_name)))
+        await bot.send_message(chat_id=7396564931, text=LEXICON["/new_member"].format(str(new_member.username), str(new_member.first_name), str(datetime.now()), str(new_member.id) ))
 
 @router.message(F.content_type.in_({'left_chat_member'}))
 async def left_member_process(message: types.Message, bot: Bot):
     await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
 
 async def main():
-    token = TOKEN
     bot = Bot(TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     dp = Dispatcher()
 
